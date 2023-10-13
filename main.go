8c3b6834ef52b25e426996fc371629d26799617f
pdaambrosio <pda.ambrosio@gmail.com>
2023-10-12 23:07:41 -0300

    refact: using packages

--- main.go
+++ main.go
@@ -1,29 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"os"
 	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"coffeShop/menu"
 )
-
-// The above code defines a struct type called `menuItem` with a name field and a prices field that is
-// a map of string keys to float64 values.
-// @property {string} name - The name property is a string that represents the name of the menu item.
-// @property prices - The `prices` property is a map that associates a string key with a float64 value.
-// It is used to store the prices of the menu item for different variations or options.
-type menuItem struct {
-	name string
-	prices map[string]float64
-}
-
-// The `menu` variable is a slice of `menuItem` structs. Each `menuItem` struct represents an item on
-// the menu and contains two fields: `name` and `prices`.
-var menu = []menuItem{
-	{name: "Coffe", prices: map[string]float64{"small": 1.50, "medium": 2.00, "large": 2.50}},
-	{name: "Tea", prices: map[string]float64{"small": 1.00, "medium": 1.50, "large": 2.00}},
-	{name: "Water", prices: map[string]float64{"small": 0.50, "medium": 1.00, "large": 1.25}},
-}
 
 // `in := bufio.NewReader(os.Stdin)` is creating a new `bufio.Reader` object called `in` that reads
 // input from the standard input (`os.Stdin`). This allows the program to read user input from the
@@ -31,7 +15,7 @@ var menu = []menuItem{
 var in = bufio.NewReader(os.Stdin)
 
 func main() {
-// The `loop` label is used to break out of the outer for loop when the user chooses to quit the program.
+	// The `loop` label is used to break out of the outer for loop when the user chooses to quit the program.
 loop:
 	// The for loop is used to repeatedly prompt the user for a menu option until they choose to quit.
 	for {
@@ -44,30 +28,14 @@ loop:
 		// The code you provided is a switch statement that handles the user's menu choice.
 		switch strings.TrimSpace(choice) {
 		case "1":
-			printMenu()
+			menu.Print()
 		case "2":
-			addItem()
+			menu.Add()
 		case "q":
 			fmt.Println("Goodbye!")
 			break loop
 		default:
 			fmt.Println("Invalid option")
-		}
-	}
-}
-
-func addItem() {
-	fmt.Println("Please enter the name of the item: ")
-	name, _ := in.ReadString('\n')
-	menu = append(menu, menuItem{name:name, prices:make(map[string]float64)})
-}
-
-func printMenu() {
-	for _, item := range menu {
-		fmt.Println(item.name)
-		fmt.Println(strings.Repeat("-", 10))
-		for size, price := range item.prices {
-			fmt.Printf("\t%10s%10.2f\n", size, price) // see https://pkg.go.dev/fmt#hdr-Printing
 		}
 	}
 }
