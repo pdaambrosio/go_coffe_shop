5e4af6ae5b6dacb6bd92ce3e12f4914eb9adba2e
pdaambrosio <pda.ambrosio@gmail.com>
2023-10-13 15:42:14 -0300

    refact: using methods

--- menu/data.go
+++ menu/data.go
@@ -2,7 +2,7 @@
 package menu
 
 // Variable "menu" is a slice of "menuItem" structs.
-var data = []menuItem{
+var data = menu{
 	{name: "Coffe", prices: map[string]float64{"small": 1.50, "medium": 2.00, "large": 2.50}},
 	{name: "Tea", prices: map[string]float64{"small": 1.00, "medium": 1.50, "large": 2.00}},
 	{name: "Water", prices: map[string]float64{"small": 0.50, "medium": 1.00, "large": 1.25}},
--- menu/menu.go
+++ menu/menu.go
@@ -14,22 +14,34 @@ type menuItem struct {
 	prices map[string]float64
 }
 
-var in = bufio.NewReader(os.Stdin) // see https://pkg.go.dev/bufio#NewReader
+type menu []menuItem
 
-// Function "add" adds a new item to the menu.
-func Add() {
-	fmt.Println("Please enter the name of the item: ")
-	name, _ := in.ReadString('\n')
-	data = append(data, menuItem{name: name, prices: make(map[string]float64)})
-}
-
-// Function "Print" prints the menu.
-func Print() {
-	for _, item := range data {
+// Method "print" contains the logic to print the menu.
+func (m menu) print() {
+	for _, item := range m {
 		fmt.Println(item.name)
 		fmt.Println(strings.Repeat("-", 10))
 		for size, price := range item.prices {
 			fmt.Printf("\t%10s%10.2f\n", size, price) // see https://pkg.go.dev/fmt#hdr-Printing
 		}
 	}
+}
+
+// Method "add" contains the logic to add a new item to the menu.
+func (m *menu) add() {
+	fmt.Println("Please enter the name of the item: ")
+	name, _ := in.ReadString('\n')
+	data = append(*m, menuItem{name: name, prices: make(map[string]float64)})
+}
+
+var in = bufio.NewReader(os.Stdin) // see https://pkg.go.dev/bufio#NewReader
+
+// Function "add" adds a new item to the menu.
+func Add() {
+	data.add()
+}
+
+// Function "Print" prints the menu.
+func Print() {
+	data.print()
 }
